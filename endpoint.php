008405a4defc797a45d835fe47eddd5c32e83fe5
SirArlex <88986261+SirArlex@users.noreply.github.com>
2023-09-11 13:05:32 -0700

    Add files via upload

---
+++ endpoint.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_GET['slack_name']) && isset($_GET['track'])) {
+    // Retrieve the values in the GET request
+    $slack_name = $_GET['slack_name'];
+    $track = $_GET['track'];
+	// Assign status code
+	
+	
+	$currentDayOfWeek = gmdate('l');
+	$utcTime = gmdate("Y-m-d\TH:i:s\Z");
+	$githubFileUrl = '';
+	$githubRepoUrl = 'https://github.com/SirArlex/Task-1';
+
+	$response = array(
+    'slack_name' => $slack_name,
+    'current_day' => $currentDayOfWeek,
+    'utc_time' => $utcTime,
+	'track' => $track,
+    'github_file_url' => $githubFileUrl,
+    'github_repo_url' => $githubRepoUrl,
+    'status_code' => 200
+);
+    header('Content-Type: application/json');
+
+    echo json_encode($response);
+} else {
+    // error message
+    $errorResponse = array('error' => 'Slack name and track are required.');
+    header('Content-Type: application/json');
+    echo json_encode($errorResponse);
+}
+?>
